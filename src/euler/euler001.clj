8428f529f550d10ee47a951022dca0110a3262b6
Soumendra Daas <soumendra.daas@gmail.com>
2017-01-30 16:07:35 +0530

    euler001 - renamed multiple() to sum-of-multiples()
    This code is functionally correct. But the performance is not good.
    Specifically test cases 2 and 3 are timing out.

--- src/euler/euler001.clj
+++ src/euler/euler001.clj
@@ -1,5 +1,6 @@
 (ns euler.euler001
 	(use [clojure.string :only (split join triml)])
+	(use [clojure.set :only (union)])
 	(:gen-class))
 
 ; Euler Challenge #1 : Multiples of 3 and 5
@@ -7,7 +8,7 @@
 ;
 ; Find the sum of all the multiples of 3 or 5 below N
 
-(defn multiple
+(defn sum-of-multiples
 	"Sum of all multiples of 3 and 5 below n"
 	[n]
 	(let [
@@ -16,7 +17,34 @@
 		]
 		(reduce + multiples)))
 
+; The slower approach. Turns out that this is 3x slower than sum-of-multiples
+(defn slow-sum-of-multiples
+	"Sum of all multiples of 3 and 5 below n"
+	[n]
+	(let [
+		multiples-3  (into #{} (range 3 n 3))
+		multiples-5  (into #{} (range 5 n 5))
+		multiples-of-both (union multiples-3 multiples-5)
+		]
+		(reduce + multiples-of-both)))
+
+(defn read-n
+	"read n elements from stdin into a vector"
+	[n]
+	(loop [
+			x n 
+			acc []
+		]
+		(if (= x 0)
+			acc
+			(recur (dec x) (conj acc (read-line))))))
+
 (defn -main
-	"I don't do a whole lot ... yet."
+	"The main ..."
 	[& args]
-	(println "Hello, World!"))
+	(let [
+		number-of-inputs  (Integer/parseInt (read-line))
+		inputs 		(map #(Integer/parseInt %) (read-n number-of-inputs))
+		multiples 	(map #(sum-of-multiples %) inputs)
+		]
+		(doseq [item multiples] (println item))))
--- test/euler/euler001_test.clj
+++ test/euler/euler001_test.clj
@@ -16,10 +16,10 @@
 
 (deftest multiple-test
 	(testing "sum of all numbers below N that are multiples of 3 or 5"
-		(is (= 0 (multiple 2)))
-		(is (= 0 (multiple 3)))  ; _below_ 3 => not including 3
-		(is (= 3 (multiple 4)))
-		(is (= 3 (multiple 5)))
-		(is (= 8 (multiple 6)))
-		(is (= 23 (multiple 10)))
-		(is (= 2318 (multiple 100)))))
+		(is (= 0 (sum-of-multiples 2)))
+		(is (= 0 (sum-of-multiples 3)))  ; _below_ 3 => not including 3
+		(is (= 3 (sum-of-multiples 4)))
+		(is (= 3 (sum-of-multiples 5)))
+		(is (= 8 (sum-of-multiples 6)))
+		(is (= 23 (sum-of-multiples 10)))
+		(is (= 2318 (sum-of-multiples 100)))))
